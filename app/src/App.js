be8da155faef73821bebddc529fe22a57618b34b
FarrukhTahir5 <farrukhtahir5@gmail.com>
2023-05-29 00:40:27 +0500

    commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,14 @@
-import React from 'react';
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-import 'firebase/database';
 import Dictaphone from './Dictaphone';
+import React from 'react';
+import './App.css';
 
-class App extends React.Component {
-  componentDidMount() {
-    // Initialize Firebase
-    const firebaseConfig = {
-      apiKey: "AIzaSyBmoCJJ-H4YkOMnb7J-L_UNX4M3jxCh9-M",
-      authDomain: "iotproject101-5209a.firebaseapp.com",
-      databaseURL: "https://iotproject101-5209a-default-rtdb.firebaseio.com",
-      projectId: "iotproject101-5209a",
-      storageBucket: "iotproject101-5209a.appspot.com",
-      messagingSenderId: "926892985236",
-      appId: "1:926892985236:web:d9f3251476d3691f4f9d81",
-      measurementId: "G-WET4YBE9T3"
-    };
-
-    const apip=initializeApp(firebaseConfig);
-    this.database = apip.database();
-  }
-
-  
-
-  render() {
-    return (
-      <div>
+function App() {
+  return (
+    <div className="App">
+      <h1>Voice Controlled Lights</h1>
         <Dictaphone/>
-        <button onClick={this.convertSpeechToText}>Convert Voice to Text</button>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
--- app/src/Dictaphone.js
+++ app/src/Dictaphone.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-const turnLightOn = () => {
-    // Update the value of /light1 to true
-    this.database.ref('light1').set(true);
-  }
 
- const turnLightOff = () => {
-    // Update the value of /light1 to false
-    this.database.ref('light1').set(false);
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyBmoCJJ-H4YkOMnb7J-L_UNX4M3jxCh9-M",
+  authDomain: "iotproject101-5209a.firebaseapp.com",
+  databaseURL: "https://iotproject101-5209a-default-rtdb.firebaseio.com",
+  projectId: "iotproject101-5209a",
+  storageBucket: "iotproject101-5209a.appspot.com",
+  messagingSenderId: "926892985236",
+  appId: "1:926892985236:web:d9f3251476d3691f4f9d81",
+  measurementId: "G-WET4YBE9T3"
+};
+
+const apip = initializeApp(firebaseConfig);
+const db = getDatabase(apip);
+
+const turnLightOn = () => {
+  // Update the value of /light1 to true
+  set(ref(db, '/light1'), {
+    light1: true,
+  });
+}
+
+const turnLightOff = () => {
+  // Update the value of /light1 to false
+  set(ref(db, '/light1'), {
+    light1: false,
+  });
+}
+
 const Dictaphone = () => {
   const {
     transcript,
@@ -17,17 +39,22 @@ const Dictaphone = () => {
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+  useEffect(() => {
+    if (transcript === "turn on light") {
+      console.log("light on registered");
+      turnLightOn();
+      SpeechRecognition.stopListening();
+    } else if (transcript === "turn off light") {
+      console.log("light off registered");
+      turnLightOff();
+      SpeechRecognition.stopListening();
+    }
+  }, [transcript]);
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
-  console.log(transcript);
-  if (transcript === "turn on light") {
-    console.log("light on registered");
-    turnLightOn();
-  } else if (transcript === "turn off light") {
-    console.log("light off registered");
-    turnLightOff();
-  }
+
   return (
     <div>
       <p>Microphone: {listening ? 'on' : 'off'}</p>
@@ -38,4 +65,5 @@ const Dictaphone = () => {
     </div>
   );
 };
+
 export default Dictaphone;
